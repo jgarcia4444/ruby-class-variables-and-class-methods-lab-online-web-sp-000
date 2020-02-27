c86eeae8a44e6ec46140d337edb824c254c3ee3a
Jake Garcia <bballwiz4@gmail.com>
2020-02-27 07:24:48 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,30 @@
+class Song
+
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+
+  @@genres = []
+  @@artists = []
+
+  def initialize(name, artist, genre)
+
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    return @@genres.sort do |a, b|
+      a != b
+    end
+  end
+
+end
